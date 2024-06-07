ed116d36628bc68496889dec69e8bc76b3ea075e
tarun.tiwari <taruntiwari085@gmail.com>
2024-06-07 23:54:11 +0530

    enhanced todo app using useRef hook

--- src/components/AddFile.jsx
+++ src/components/AddFile.jsx
@@ -1,29 +1,24 @@
-import { useState } from "react";
+import { useRef } from "react";
 
 function AddFile({onaddItem}){
 
-  const [addItem,setAddItem]=useState();
-  const [dueDate,setDueDate]=useState();
-
-  const handleNewItem = (event) => {
-    setAddItem(event.target.value);
-  }
-  const handledueDate = (event) => {
-    setDueDate(event.target.value);
-  }
+  const todoName = useRef();
+  const todoDueDate = useRef();
   const handleAddButtonClicked = ()=>{
+    const addItem = todoName.current.value;
+    const dueDate = todoDueDate.current.value;
+    todoName.current.value = "";
+    todoDueDate.current.value = "";
     onaddItem(addItem,dueDate);
-    setAddItem("");
-    setDueDate("");
   }
 
   return <div class="container">
   <div class="row my-row">
     <div class="col-6">
-      <input type="text" placeholder="Enter yout to do task" value={addItem} onChange={handleNewItem}></input>
+      <input type="text" placeholder="Enter yout to do task" ref={todoName}></input>
     </div>
     <div class="col-4">
-      <input type="date" value={dueDate} onChange={handledueDate}></input>
+      <input type="date" ref={todoDueDate}></input>
     </div>
     <div class="col-2">
     <button type="button" class="btn btn-success my-button"  onClick={handleAddButtonClicked}>Add</button>
